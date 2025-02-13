af4657e886a7d25958431c07fce6c7b9035ac57c
FelipeGabrill <felipegabrielodss@gmail.com>
2025-02-13 16:32:58 -0300

    Course

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJEJX2OFLWTPzUGOXyrMR4f88JyDPaf6uA2A&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrWjebs2US03nck3BE3QoQR6MxXGdBIPE7Og&s');
